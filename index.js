08e1cce99a1267b38ac8e7e24708fb05effb342f
jatinrwt01 <work.jatinynk@gmail.com>
2026-08-07 13:26:39 +0530

    connect discord bot and handle ready event

---
+++ index.js
@@ -0,0 +1,18 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent
+    ]
+})
+
+client.login(process.env.DISCORD_BOT_TOKEN);
+
+client.on("clientReady", ()=>{
+    console.log("Bot is online!");
+})
