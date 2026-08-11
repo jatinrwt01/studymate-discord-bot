efdfd632568d5e31ef977fc86242fdf4e870842d
jatinrwt01 <work.jatinynk@gmail.com>
2026-08-11 12:59:37 +0530

    add automatic command loader

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
-import { ping } from "./commands/ping.js";
-import { hello } from "./commands/hello.js";
+import fs from "fs";
 
 dotenv.config();
 
@@ -13,29 +12,42 @@ const client = new Client({
     ]
 })
 
-client.login(process.env.DISCORD_BOT_TOKEN);
-
-client.on("clientReady", ()=>{
-    console.log("Bot is online!");
-})
-
-
 const commands = {};
-commands[ping.name] = ping;
-commands[hello.name] = hello;
-client.on("messageCreate", (msg)=>{
-    handleCommand(msg);
-})
+
+async function loadCommands(){
+    const files = fs.readdirSync("./commands");
+for(const file of files){
+     const module = await import(`./commands/${file}`);
+    const commandObj = Object.values(module)[0];
+    commands[commandObj.name] = commandObj;
+}
+}
 
 function handleCommand(msg){
     if(msg.author.bot) return;
     const command = commands[msg.content];
-    console.log(command);
     if(command){
         return command.execute(msg);
     }
 }
 
+
+async function startBot(){
+    await loadCommands();
+
+    client.on("clientReady", ()=>{
+    console.log("Bot is online!");
+})
+
+     client.on("messageCreate", (msg)=>{
+    handleCommand(msg);
+})
+
+    await client.login(process.env.DISCORD_BOT_TOKEN);  
+    console.log(commands); 
+}
+
+startBot();
 
 
 
